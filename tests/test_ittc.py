5c0e363872898a1381b4c3f56f65bd6c7a80d939
Giuseppe Latini <giuseppe.latini@gmail.com>
2024-09-29 15:47:38 +0200

    Primo commit indice ufficiale, release 0.0.1

---
+++ tests/test_ittc.py
@@ -0,0 +1,15 @@
+from src.ittaxcode import ittc as cf
+
+
+def test():
+    assert cf.check(
+        "PPPNNNLMBMQLMMTA"
+    )
+
+    assert cf.age(
+        "PPPNNNLMBMQLMMTA"
+    ) == 23
+
+    assert str(cf.birthdate(
+        "PPPNNNLMBMQLMMTA"
+    )) == "2001-02-14"
